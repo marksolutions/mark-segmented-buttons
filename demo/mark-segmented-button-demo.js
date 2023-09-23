4d4dfa61fcf72061d915056415e58c7945359ce2
Hiten Bhayani <hitenbhayani41@gmail.com>
2023-09-23 21:37:11 +0530

    project setup

---
+++ demo/mark-segmented-button-demo.js
@@ -0,0 +1,17 @@
+import { LitElement, html, css } from 'lit';
+import '../src/mark-segmented-button';
+
+export class MarkSegmentedButtonDemo extends LitElement {
+  static styles = [
+    css`
+      :host {
+        display: block;
+      }
+    `
+  ];
+
+  render() {
+    return html`<mark-segmented-button></mark-segmented-button>`;
+  }
+}
+customElements.define('mark-segmented-button-demo', MarkSegmentedButtonDemo);
